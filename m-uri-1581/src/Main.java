ccd8d03f6aa4b7da86d7f8174e0e3d5ec75e2c88
Orozkul Oktonov <oktonov_o@auca.kg>
2021-01-30 16:08:07 +0600

    uri p-1581 (runtime error) I can't find mistake

---
+++ m-uri-1581/src/Main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int t = inp.nextInt();
+        for (int i=0; i<t; i++){
+            boolean english = false;
+            int n= inp.nextInt();
+            String firstSpeaker = inp.next();
+            for (int j=1; j<n; j++){
+                String s = inp.next();
+                if (!s.equals(firstSpeaker)){
+                    english = true;
+                    break;
+                }
+            }
+            if (english){
+                System.out.println("ingles");
+            } else {
+                System.out.println(firstSpeaker);
+            }
+        }
+    }
+}
