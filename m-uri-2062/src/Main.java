2da01e67cc34d4ab73b83701a485fd29e6105235
Orozkul Oktonov <oktonov_o@auca.kg>
2021-02-07 23:38:45 +0600

    uri p-2062 (Presentation error)

---
+++ m-uri-2062/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int n = inp.nextInt();
+        for (int i=0; i<n; i++){
+            String s = inp.next();
+            StringBuilder ans = new StringBuilder();
+            if (s.length()==3 && (s.charAt(0)=='U' && s.charAt(1)=='R' || s.charAt(0)=='O' && s.charAt(1)=='B')){
+                ans.append(s.charAt(0));
+                ans.append(s.charAt(1));
+                ans.append('I');
+            } else {
+                ans.append(s);
+            }
+            System.out.print(ans+" ");
+        }
+        System.out.println();
+    }
+}
