526c4a2c9c7b2e6d833251b10cf719ecfe9cc69e
Orozkul Oktonov <oktonov_o@auca.kg>
2021-02-07 21:59:01 +0600

    uri p-1237 introduction

---
+++ m-uri-1237/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        while (inp.hasNext()){
+            String s1 = inp.next();
+            String s2 = inp.next();
+        }
+    }
+}
+
