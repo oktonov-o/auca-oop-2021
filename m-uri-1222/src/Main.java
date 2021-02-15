d3f03b0e947f30abd8a88a4574d86f4a4f93fc1c
Orozkul Oktonov <oktonov_o@auca.kg>
2021-02-16 00:05:17 +0600

    uri p-1222 (wrong answer 5%)

---
+++ m-uri-1222/src/Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        while (inp.hasNext()){
+            int n = inp.nextInt();
+            int l = inp.nextInt();
+            int c = inp.nextInt();
+            int chars = 0;
+            int lines = 0;
+            for (int i=0; i<n; i++){
+                String s = inp.next();
+                if (chars + s.length() > c){
+                    chars=s.length()+1;
+                    ++lines;
+                    if(i==n-1){
+                        ++lines;
+                    }
+                }else if(i==n-1){
+                    lines+=2;
+                }else  {
+                    chars+=s.length()+1;
+                }
+            }
+            System.out.println((int)Math.ceil(1.0*(lines)/l));
+        }
+    }
+}
