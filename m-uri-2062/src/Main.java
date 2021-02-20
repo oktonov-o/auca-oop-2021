1b2e39349b3d1f27c9aa0a8ef5660af3e6256fbe
Orozkul Oktonov <oktonov_o@auca.kg>
2021-02-20 17:11:22 +0600

    uri p-2062 accepted

--- m-uri-2062/src/Main.java
+++ m-uri-2062/src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int n = inp.nextInt();
-        for (int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             String s = inp.next();
             StringBuilder ans = new StringBuilder();
-            if (s.length()==3 && (s.charAt(0)=='U' && s.charAt(1)=='R' || s.charAt(0)=='O' && s.charAt(1)=='B')){
+            if (s.length() == 3 && (s.charAt(0) == 'U' && s.charAt(1) == 'R' || s.charAt(0) == 'O' && s.charAt(1) == 'B')) {
                 ans.append(s.charAt(0));
                 ans.append(s.charAt(1));
                 ans.append('I');
@@ -15,7 +15,7 @@ public class Main {
                 ans.append(s);
             }
             System.out.print(ans);
-            if(i!=n-1){
+            if (i != n - 1) {
                 System.out.print(" ");
             }
         }
