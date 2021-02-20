f8d8c9ceed3c9641a60fb6fc331af504172dcdb5
Orozkul Oktonov <oktonov_o@auca.kg>
2021-02-20 16:23:09 +0600

    uri p-1239 accepted

--- m-uri-1239/src/Main.java
+++ m-uri-1239/src/Main.java
@@ -3,25 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        while(inp.hasNext()){
+        while(inp.hasNextLine()){
             String s = inp.nextLine();
             StringBuilder ans = new StringBuilder();
-            int cnt=0;
+            int cntI=0, cntB = 0;
             for (int i=0; i<s.length(); i++){
                 if (s.charAt(i) == '_'){
-                    if (cnt%2==0){
+                    if (cntI%2==0){
                         ans.append("<i>");
                     } else {
                         ans.append("</i>");
                     }
-                    cnt++;
+                    cntI++;
                 } else if (s.charAt(i) == '*'){
-                    if (cnt%2==0){
+                    if (cntB%2==0){
                         ans.append("<b>");
                     } else {
                         ans.append("</b>");
                     }
-                    cnt++;
+                    cntB++;
                 } else {
                     ans.append(s.charAt(i));
                 }
