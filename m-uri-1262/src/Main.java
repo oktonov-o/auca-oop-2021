c6dc96841bae7038177ed8e1cf414b8dc45344b0
Orozkul Oktonov <oktonov_o@auca.kg>
2021-02-20 18:43:58 +0600

    uri p-1262 in a queue

---
+++ m-uri-1262/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int p, i, proc, clock;
+
+        while(inp.hasNext()){
+            String c = inp.next();
+            p = inp.nextInt();
+            proc = clock = 0;
+            for(i = 0; i< c.length(); i++){
+                if(c.charAt(i) == 'W'){
+                    clock++;
+                    if(proc > 0){
+                        proc = 0;
+                        clock++;
+                    }
+                }
+                else{
+                    proc++;
+                    if(proc == p){
+                        clock++;
+                        proc = 0;
+                    }
+                }
+            }
+            if(proc > 0) clock ++;
+            System.out.println(clock);
+        }
+    }
+}
