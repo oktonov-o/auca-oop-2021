fb74c4f8d72cbb43c2ca250444c94f07b4c1ee94
Orozkul Oktonov <oktonov_o@auca.kg>
2021-03-01 21:37:00 +0600

    uri p-1263 accepted

--- m-uri-1262/src/Main.java
+++ m-uri-1262/src/Main.java
@@ -5,27 +5,26 @@ public class Main {
         Scanner inp = new Scanner(System.in);
         int p, i, proc, clock;
 
-        while(inp.hasNext()){
+        while (inp.hasNext()) {
             String c = inp.next();
             p = inp.nextInt();
             proc = clock = 0;
-            for(i = 0; i< c.length(); i++){
-                if(c.charAt(i) == 'W'){
+            for (i = 0; i < c.length(); i++) {
+                if (c.charAt(i) == 'W') {
                     clock++;
-                    if(proc > 0){
+                    if (proc > 0) {
                         proc = 0;
                         clock++;
                     }
-                }
-                else{
+                } else {
                     proc++;
-                    if(proc == p){
+                    if (proc == p) {
                         clock++;
                         proc = 0;
                     }
                 }
             }
-            if(proc > 0) clock ++;
+            if (proc > 0) clock++;
             System.out.println(clock);
         }
     }
