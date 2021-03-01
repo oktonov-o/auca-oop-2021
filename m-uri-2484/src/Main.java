3e5356be754e3ced69a42df372f66f0e882be36f
Orozkul Oktonov <oktonov_o@auca.kg>
2021-03-02 00:11:11 +0600

    uri p-2484 accepted

--- m-uri-2484/src/Main.java
+++ m-uri-2484/src/Main.java
@@ -4,15 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         String s;
-        while (inp.hasNext()){
+        while (inp.hasNext()) {
             s = inp.next();
-            for (int row = 0; row<s.length(); row++){
-                for (int col=0; col<s.length(); col++){
-                    if(col<row){
+            for (int row = 0; row < s.length(); row++) {
+                for (int col = 0; col < s.length(); col++) {
+                    if (col < row) {
                         System.out.print(" ");
                     } else {
-                        System.out.print(s.charAt(col-row));
-                        if (col+1 == s.length()){
+                        System.out.print(s.charAt(col - row));
+                        if (col + 1 == s.length()) {
                             break;
                         } else {
                             System.out.print(" ");
