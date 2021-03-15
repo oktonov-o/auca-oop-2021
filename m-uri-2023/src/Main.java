8c45ff29d655d2f2ae1087550fb9201db7c6763e
Orozkul Oktonov <oktonov_o@auca.kg>
2021-03-15 19:35:02 +0600

    uri p-2023 accepted

--- m-uri-2023/src/Main.java
+++ m-uri-2023/src/Main.java
@@ -1,20 +1,21 @@
-import java.util.Arrays;
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    private static String[] names = new String[1001];
+
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
-        int index=0;
+        ArrayList<String> names = new ArrayList<String>();
+
         while (inp.hasNextLine()) {
-            names[index] = inp.nextLine();
-            index++;
+            String c = inp.nextLine();
+            names.add(c);
         }
-        String[] names2 = new String[index];
-        for (int i=0; i<index; i++){
-            names2[i] = names[i];
-        }
-        Arrays.sort(names2);
-        System.out.println(names2[index-1]);
+        Collections.sort(names, Collator.getInstance(new Locale("es")));
+
+        System.out.println(names.get(names.size() - 1));
     }
 }
