86955fff90a3d44ed45bc4e7fa6be2506fda8f7d
Orozkul Oktonov <oktonov_o@auca.kg>
2021-03-17 00:11:13 +0600

    lab-04 Problem04 solved

---
+++ lab-04/src/Problem05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Problem05 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        MyArrayList arr = new MyArrayList();
+
+        while (inp.hasNextInt()) {
+            int x = inp.nextInt();
+            arr.add(x);
+        }
+
+        for (int i = 0; i < arr.size(); ) {
+            if (arr.get(i) % 2 == 0) {
+                arr.add(i, 0);
+                i += 2;
+            } else {
+                ++i;
+            }
+        }
+        System.out.println("After insertions: ");
+        System.out.println(arr);
+
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i) % 2 != 0) {
+                arr.remove(i);
+                i--;
+            }
+        }
+        System.out.println("After removing: ");
+        System.out.println(arr);
+    }
+}
