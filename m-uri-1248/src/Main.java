add89785c4e0a459f4c56ac2d1bb29c31a5239c8
Orozkul Oktonov <oktonov_o@auca.kg>
2021-03-17 01:05:48 +0600

    uri p-1248 accepted

--- m-uri-1248/src/Main.java
+++ m-uri-1248/src/Main.java
@@ -1,17 +1,38 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         int tests = inp.nextInt();
+        inp.nextLine();
         for (int test=0; test<tests; test++){
             StringBuilder eaten = new StringBuilder();
-            StringBuilder list = new StringBuilder();
-            list.append(inp.nextLine());
+            ArrayList <String> newList = new ArrayList<String>();
+            String list = inp.nextLine();
             eaten.append(inp.nextLine());
             eaten.append(inp.nextLine());
-            System.out.println(list);
+            for (int i=0; i<list.length(); i++){
+                if (eaten.indexOf(Character.toString(list.charAt(i))) < 0){
+                    newList.add(Character.toString(list.charAt(i)));
+                } else {
+                    eaten.deleteCharAt(eaten.indexOf(Character.toString(list.charAt(i))));
+                }
+            }
+            if (eaten.length() != 0){
+                System.out.println("CHEATER");
+            } else {
+                String[] answer = new String[newList.toArray().length];
+                for(int i=0; i<answer.length; i++){
+                    answer[i] = newList.get(i);
+                }
+                Arrays.sort(answer);
+                for(int i=0; i<answer.length; i++){
+                    System.out.print(answer[i]);
+                }
+                System.out.println();
+            }
         }
     }
 }
