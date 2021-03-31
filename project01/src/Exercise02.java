35abb9ef79b61e8b9ff3cbd6aef14ba9eb5e4562
Orozkul Oktonov <oktonov_o@auca.kg>
2021-03-31 14:07:13 +0600

    Project01 Exercise02 solved

---
+++ project01/src/Exercise02.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Exercise02 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int m = inp.nextInt();
+        int n = inp.nextInt();
+        System.out.println(gcd(m, n));
+    }
+
+    private static int gcd(int m, int n) {
+        if (m%n == 0){
+            return n;
+        } else {
+            return gcd(n, m%n);
+        }
+    }
+}
