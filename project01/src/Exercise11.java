1c7069cb14d566899347c48f3148596e9c23365e
Orozkul Oktonov <oktonov_o@auca.kg>
2021-04-03 14:40:14 +0600

    project01 exercise11 solved

---
+++ project01/src/Exercise11.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Exercise11 {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        long n = inp.nextLong();
+        System.out.println(sumDigits(n));
+    }
+
+    public static int sumDigits (long n){
+        if (n == 0){
+            return 0;
+        } else {
+            return (int)(n%10 + sumDigits(n/10));
+        }
+    }
+}
