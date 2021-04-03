ec8087f3ae69a508d8a0c5118dc2fc68db94ad0b
Orozkul Oktonov <oktonov_o@auca.kg>
2021-04-03 13:55:39 +0600

    uri p-1218 (presentation error 5%)

--- f-uri-1218/src/Main.java
+++ f-uri-1218/src/Main.java
@@ -5,23 +5,23 @@ public class Main {
         Scanner inp = new Scanner(System.in);
         int size, male, female, day = 1;
         String s;
-        while (inp.hasNextLine()){
+        while (inp.hasNextLine()) {
             male = 0;
             female = 0;
             size = inp.nextInt();
             inp.nextLine();
             s = inp.nextLine();
-            while (s.indexOf(Integer.toString(size)) != -1){
-                if (s.charAt(s.indexOf(Integer.toString(size))+Integer.toString(size).length()+1) == 'F'){
+            while (s.contains(Integer.toString(size))) {
+                if (s.charAt(s.indexOf(Integer.toString(size)) + Integer.toString(size).length() + 1) == 'F') {
                     female++;
-                }else {
+                } else {
                     male++;
                 }
                 s = deleteCharAt(s, s.indexOf(Integer.toString(size)));
             }
             System.out.printf("Caso %d:%n", day);
             day++;
-            System.out.printf("Pares Iguais: %d%n", male+female);
+            System.out.printf("Pares Iguais: %d%n", male + female);
             System.out.printf("F: %d%n", female);
             System.out.printf("M: %d%n", male);
         }
