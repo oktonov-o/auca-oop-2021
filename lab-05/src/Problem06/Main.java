e3f1b8f62130cbffd8afa3533e94d5f48c9af20f
Orozkul Oktonov <oktonov_o@auca.kg>
2021-04-12 21:18:35 +0600

    lab-05 Problem06 solved

---
+++ lab-05/src/Problem06/Main.java
@@ -0,0 +1,34 @@
+package Problem06;
+
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import javax.swing.*;
+
+public class Main extends JFrame {
+
+    JPanel mainPanel;
+
+    Main () {
+        mainPanel = new JPanel();
+        mainPanel.setBackground(Color.DARK_GRAY);
+        add(mainPanel, BorderLayout.CENTER);
+
+        mainPanel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+
+                setTitle(e.getX() + ", " + e.getY());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+
+        Main frame = new Main();
+        frame.setTitle("Move mouse");
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
