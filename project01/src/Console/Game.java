ac68cae23786c9f1b1dbea682a1bde4e890fb7a9
Orozkul Oktonov <oktonov_o@auca.kg>
2021-05-17 17:03:00 +0600

    Project01 minesweeper-console version solved

--- project01/src/Console/Game.java
+++ project01/src/Console/Game.java
@@ -1,32 +1,59 @@
 package Console;
 
+import Help.Flag;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Game {
-    int height;
-    int width;
-    int mines;
-    int[][] data;
 
 
-    public Game (int height, int width, int mines){
+    private static final int[] dr = {-1, -1, 0, 1, 1, 1, 0, -1};
+    private static final int[] dc = {0, 1, 1, 1, 0, -1, -1, -1};
+
+    private boolean firstTime = true;
+    private int height;
+    private int width;
+    private int numOfMines;
+    private int numOfFlagsLeft;
+    private char[][] data;
+    private char[][] numData;
+    private ArrayList<Flag> flags = new ArrayList<>();
+    private boolean[][] revealed;
+    public boolean lost = false;
+    public boolean won = false;
+    private int nCorrectFlags;
+
+
+    public Game (int height, int width, int numOfMines){
         if (height < 1){
             throw new RuntimeException("Game: height is less than one: " + height);
         }
         if (width < 1){
             throw new RuntimeException("Game: width is less than one: " + width);
         }
-        if (mines < 1 || mines >= height*width){
-            throw new RuntimeException("Game: incorrect number of mines: " + mines);
+        if (numOfMines < 1 || numOfMines >= height*width){
+            throw new RuntimeException("Game: incorrect number of mines: " + numOfMines);
         }
         this.height = height;
         this.width = width;
-        this.mines = mines;
+        this.numOfMines = numOfMines;
+        numOfFlagsLeft = numOfMines;
+        nCorrectFlags = numOfMines;
 
-        this.data = new int[height][width];
+        data = new char[height][width];
+        numData = new char[height][width];
+        revealed = new boolean[height][width];
+        for (int i=0; i<height; i++){
+            for (int j=0; j<width; j++){
+                data[i][j] = '.';
+            }
+        }
     }
 
     @Override
     public String toString() {
-        String header = String.format("Mine Sweeper: height: %d, width = %d, mines = %d%n", height, width, mines);
+        String header = String.format("Mine Sweeper: height: %d, width = %d, mines = %d%n", height, width, numOfMines);
         StringBuilder r = new StringBuilder(header);
 
         for (int i=0; i<height; i++){
@@ -46,8 +73,109 @@ public class Game {
             throw new RuntimeException("Out of range");
         }
 
-        // if this is the first left click then put mines
+        if (firstTime){
+            init(row, col);
+            firstTime = false;
+        }
+        if (numData[row][col] == '*'){
+            lost = true;
+        } else {
+            if (flags.contains(new Flag(row, col))){
+                flags.remove(new Flag(row, col));
+                data[row][col] = '.';
+                ++numOfFlagsLeft;
+            }
+            reveal(row, col);
+        }
+    }
 
-        // do the rest
+    private void reveal(int row, int col) {
+        if (0 > row || row >= height || col < 0 || col >= width) return;
+        if(revealed[row][col]) return;
+        revealed[row][col] = true;
+        if(numData[row][col] != '0'){
+            data[row][col] = numData[row][col];
+            return;
+        } else {
+            data[row][col] = '0';
+        }
+        for (int i=0; i<dr.length; i++){
+            reveal(row + dr[i], col + dc[i]);
+        }
+    }
+
+    private void init(int r, int c){
+        Random random = new Random();
+        int tr, tc;
+        int m = 0;
+        while (m < numOfMines){
+            tr = random.nextInt(height);
+            tc = random.nextInt(width);
+            if (tr != r && tc != c && numData[tr][tc] != '*'){
+                numData[tr][tc] = '*';
+                m++;
+            }
+        }
+        if (firstTime){
+            initNumData();
+        }
+    }
+
+    private void initNumData() {
+        for (int r=0; r<height; r++){
+            for (int c=0; c<width; c++){
+                if (numData[r][c] != '*'){
+                    int nMines=0;
+                    for (int i=0; i<dr.length; i++){
+                        int tr = r+dr[i];
+                        int tc = c + dc[i];
+                        if (0 <= tr && tr < height && tc >= 0 && tc < width && numData[tr][tc] == '*'){
+                            ++nMines;
+                        }
+                    }
+                    numData[r][c] = (char)(48+nMines);
+                }
+            }
+        }
+    }
+
+    public void right(int row, int col) {
+        if (row < 0 || height <= row){
+            throw new RuntimeException("Out of range");
+        }
+        if (col < 0 || width <= col){
+            throw new RuntimeException("Out of range");
+        }
+        if (numOfFlagsLeft == 0){
+            throw new RuntimeException("No Flags Left");
+        }
+        Flag flag = new Flag(row, col);
+        if (flags.contains(flag)){
+            flags.remove(flag);
+            data[row][col] = '.';
+            ++numOfFlagsLeft;
+            if (numData[row][col] == '*'){
+                ++nCorrectFlags;
+            }
+        } else {
+            flags.add(flag);
+            --numOfFlagsLeft;
+            data[row][col] = 'F';
+            if (numData[row][col] == '*'){
+                --nCorrectFlags;
+            }
+        }
+        if (nCorrectFlags == 0 && numOfFlagsLeft == 0){
+            won = true;
+        }
+    }
+
+    public void show() {
+        for (int r=0; r < height; r++){
+            for (int c=0; c< width; c++){
+                System.out.print(numData[r][c]);
+            }
+            System.out.println();
+        }
     }
 }
--- project01/src/Console/Main.java
+++ project01/src/Console/Main.java
@@ -4,6 +4,19 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
+
+    private static String help = String.format("Help:\n" +
+                                        "left <row> <col>\n" +
+                                        "    - left click with the coordinates (row, col)\n" +
+                                        "right <row> <col>\n" +
+                                        "    - right click with the coordinates (row, col)\n" +
+                                        "show\n" +
+                                        "    - show all mines (cheating)\n" +
+                                        "quit\n" +
+                                        "    - quit the game (EOF works too)\n" +
+                                        "help\n" +
+                                        "    - this text\n");
+
     public static void main(String[] args) {
         Scanner inp = new Scanner(System.in);
         Game game = null;
@@ -14,26 +27,50 @@ public class Main {
             System.exit(1);
         } catch (RuntimeException e){
             System.out.println(e.getMessage());
-            System.exit(1);
+            System.exit(2);
         }
 
+        System.out.println(help);
+
         for (;;){
+            System.out.println(game);
+
             String cmdLine = inp.nextLine().trim();
+
             if(cmdLine.equals("quit")){
+                System.out.println("Game stopped");
                 break;
             }
 
             ArrayList<String> cmdArgs = new ArrayList<>();
             Scanner cmdInp = new Scanner(cmdLine);
             String firstArg = cmdInp.next();
+            int row, col;
             switch (firstArg){
                 case "left":
-                    int row = cmdInp.nextInt();
-                    int col = cmdInp.nextInt();
+                    row = cmdInp.nextInt();
+                    col = cmdInp.nextInt();
                     game.left(row, col);
                     break;
                 case "right":
+                    row = cmdInp.nextInt();
+                    col = cmdInp.nextInt();
+                    game.right(row, col);
                     break;
+                case "show":
+                    game.show();
+                    break;
+                case "help":
+                    System.out.println(help);
+                    break;
+            }
+            if (game.lost){
+                System.out.println("You lost the game");
+                break;
+            }
+            if (game.won){
+                System.out.println("Congrats!!! You WON!!!");
+                break;
             }
         }
         System.out.println("Bye");
@@ -51,6 +88,23 @@ public class Main {
             mines = Integer.parseInt(args[2]);
 
             return new Game(height, width, mines);
+        } else if (args.length == 1){
+            if (args[0].equals("expert")){
+                width = 30;
+                height = 16;
+                mines = 99;
+                return new Game(height, width, mines);
+            } else if (args[0].equals("intermediate")){
+                width = 16;
+                height = 16;
+                mines = 40;
+                return new Game(height, width, mines);
+            } else if (args[0].equals("beginner")){
+                width = 9;
+                height = 9;
+                mines = 10;
+                return new Game(height, width, mines);
+            }
         }
 
         throw new RuntimeException("Incorrect number of command line arguments!!!");
